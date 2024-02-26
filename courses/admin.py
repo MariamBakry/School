56344119193e900f3b727be740ad05e20a6747c1
MariamBakry <122481396+MariamBakry@users.noreply.github.com>
2024-02-27 01:50:56 +0200

    not active teachers and course time period
    Admin can not assign not active teacher to any course
    End date of the course must be grater than start date

--- courses/admin.py
+++ courses/admin.py
@@ -1,10 +1,19 @@
 from django.contrib import admin
 from .models import Course
-# Register your models here.
+from teachers.models import Teacher
 
 """ Check if the teacher is already assigned to a course within
     the same time frame """
 class CourseAdmin(admin.ModelAdmin):
+
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        active_teacher = Teacher.objects.exclude(user__is_active=False)
+        
+        if db_field.name == "teacher":
+            kwargs["queryset"] = active_teacher
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
+
+
     def save_model(self, request, obj, form, change):
         """get all courses of the same teacher and the same time frame
             of the current course, with excluding the current course"""
@@ -16,7 +25,7 @@ class CourseAdmin(admin.ModelAdmin):
         
         if existing_courses.exists():
             conflicting_courses = ", ".join([course.name for course in existing_courses])
-            message = f"The teacher is already assigned to the following course(s) during this time period: {conflicting_courses}"
+            message = f"The teacher is already assigned to the following course(s): [ {conflicting_courses} ] during this time period"
             self.message_user(request, message, level='ERROR')
             return
         
--- courses/models.py
+++ courses/models.py
@@ -1,8 +1,15 @@
 from django.db import models
 from django.utils import timezone
 from teachers.models import Teacher
+from django.core.exceptions import ValidationError
+from datetime import date
 
 # Create your models here.
+
+def validate_end_date_greater_than_start_date(value):
+    if value.end_date <= value.start_date:
+        raise ValidationError('End date must be greater than the start date.')
+
 
 class Course(models.Model):
     name = models.CharField(max_length=255, unique=True)
@@ -15,4 +22,7 @@ class Course(models.Model):
     def __str__(self):
         return self.name
 
+    def clean(self):
+        validate_end_date_greater_than_start_date(self)
+        return super().clean()
     
