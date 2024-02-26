eb627cb0e2656aabe86174df35e346545adc8808
MariamBakry <122481396+MariamBakry@users.noreply.github.com>
2024-02-27 01:18:49 +0200

    each teacher can only teach a single course
    each teacher can only teach a single course at a time
    admin can't assign a teacher to more than one course in the same time frame

--- courses/admin.py
+++ courses/admin.py
@@ -2,4 +2,24 @@ from django.contrib import admin
 from .models import Course
 # Register your models here.
 
-admin.site.register(Course)
+""" Check if the teacher is already assigned to a course within
+    the same time frame """
+class CourseAdmin(admin.ModelAdmin):
+    def save_model(self, request, obj, form, change):
+        """get all courses of the same teacher and the same time frame
+            of the current course, with excluding the current course"""
+        existing_courses = Course.objects.filter(
+            teacher=obj.teacher,
+            start_date__lte=obj.end_date,
+            end_date__gte=obj.start_date
+        ).exclude(pk=obj.pk)
+        
+        if existing_courses.exists():
+            conflicting_courses = ", ".join([course.name for course in existing_courses])
+            message = f"The teacher is already assigned to the following course(s) during this time period: {conflicting_courses}"
+            self.message_user(request, message, level='ERROR')
+            return
+        
+        super().save_model(request, obj, form, change)
+
+admin.site.register(Course, CourseAdmin)
