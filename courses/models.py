bedc0ae28c66d73a608cd89719364627bc31c214
MariamBakry <122481396+MariamBakry@users.noreply.github.com>
2024-02-26 06:18:33 +0200

    .

--- courses/models.py
+++ courses/models.py
@@ -1,15 +1,16 @@
 from django.db import models
 from django.utils import timezone
+from teachers.models import Teacher
 
 # Create your models here.
 
 class Course(models.Model):
     name = models.CharField(max_length=255)
-    # teacher_id = models.IntegerField(blank=True)
+    teacher = models.ForeignKey(Teacher, null=True, blank=True, on_delete=models.SET_NULL)
     description = models.TextField(blank=True)
     start_date = models.DateField(default = timezone.now().date())
     end_date = models.DateField(default = timezone.now().date())
-    active = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=False)
 
     def __str__(self):
         return self.name
