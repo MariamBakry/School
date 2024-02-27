72cd87c8b3b90b67a9c16f48f87669c7a9e8f9c8
MariamBakry <122481396+MariamBakry@users.noreply.github.com>
2024-02-27 07:07:22 +0200

    pagination in all Lists

--- courses/views.py
+++ courses/views.py
@@ -1,48 +1,19 @@
-from django.shortcuts import get_object_or_404
 from .models import *
 from rest_framework.views import APIView
 from .serializers import *
-from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
+from school.pagination import CustomCursorPagination
 # Create your views here.
 
 class CourseListView(APIView):
     permission_classes = [IsAuthenticated]
+    pagination_class = CustomCursorPagination
 
     def get(self, request):
-        courses = Course.objects.all()
-        serializer = CourseSerializer(courses, many=True)
-        return Response(serializer.data)
-    
-class GetCourseView(APIView):
-    permission_classes = [IsAuthenticated]
-    
-    def get(self, request, course_id):
-        course = get_object_or_404(Course ,pk=course_id)
-        serializer = CourseSerializer(course)
-        return Response(serializer.data)
-    
-# class CreateCourseView(APIView):
-#     def post(self, request):
-#         serializer = CourseSerializer(data = request.data)
-#         if serializer.is_valid():
-#             serializer.save()
-#             return Response(serializer.data)
-#         return Response(serializer.errors)
-    
-# class UpdateCourseView(APIView):
-#     def put(self, request, course_id):
-#         course = get_object_or_404(Course, pk=course_id)
-#         serializer = CourseSerializer(course, data=request.data)
-#         if serializer.is_valid():
-#             serializer.save()
-#             return Response(serializer.data)
-#         return Response(serializer.errors)
+        courses = Course.objects.filter(is_active=True)
 
-    
-# class DeleteCourseView(APIView):
-#     def delete(self, request, course_id):
-#         course = get_object_or_404(Course, pk=course_id)
-#         course.delete()
-#         return Response({'message': 'Course deleted successfully'})
-    
+        paginator = self.pagination_class()
+        paginated_courses = paginator.paginate_queryset(courses, request)
+
+        serializer = CourseSerializer(paginated_courses, many=True)
+        return paginator.get_paginated_response(serializer.data)
--- enrollments/views.py
+++ enrollments/views.py
@@ -1,4 +1,4 @@
-# from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 from rest_framework.views import APIView
 from rest_framework import status
 from rest_framework.response import Response
@@ -7,68 +7,76 @@ from .models import Enrollment
 from .serializers import EnrollmentSerializer, NewEnrollSerializer
 from courses.models import Course
 import datetime
+from school.pagination import CustomCursorPagination
 # Create your views here.
 
 class EnrollmentListView(APIView):
     permission_classes = [IsAuthenticated]
+    pagination_class = CustomCursorPagination
 
     def get(self, request):
-        student = request.user.student
-        enrollments = Enrollment.objects.filter(student = student)
-        serializer = EnrollmentSerializer(enrollments, many=True)
-        return Response(serializer.data)
-    
+        user = request.user
+        if user.user_type == 'student':
+            student = user.student
+            enrollments = Enrollment.objects.filter(student = student)
+            
+            paginator = self.pagination_class()
+            paginated_enrollments = paginator.paginate_queryset(enrollments, request)
+            serializer = EnrollmentSerializer(paginated_enrollments, many=True)
+            return paginator.get_paginated_response(serializer.data)
+        return Response({'message': 'it might be you are not a user of type student.'})
+        
+
 class EnrollCourseView(APIView):
     permission_classes = [IsAuthenticated]
 
     def post(self, request):
-        student = request.user.student
-        data = request.data.copy()
-        data['student'] = student
+        user = request.user
+        if user.user_type == 'student':
+            student = user.student
+            data = request.data.copy()
+            data['student'] = student
 
-        course_name = data.pop('course_name')
-        course_name = str(course_name[0])
+            course_name = data.pop('course_name')
+            course_name = str(course_name[0])
+            course = get_object_or_404(Course, name=course_name)
+            
+            if course.is_active:
+                if course.start_date > datetime.date.today():
+                    data['course'] = course.id
 
-        try:
-            course = Course.objects.get(name=course_name)
-        except Course.DoesNotExist:
-            return Response({'error': f"Course '{course_name}' does not exist"}, status=status.HTTP_400_BAD_REQUEST)
-        
-        if course.start_date > datetime.date.today():
-            data['course'] = course.id
-
-            serializer = NewEnrollSerializer(data = data)
-            if serializer.is_valid():
-                serializer.save()
-                return Response(serializer.data, status=status.HTTP_201_CREATED)
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-        return Response({'message': 'You can not Enroll the Course after the course start date.'})
-    
+                    serializer = NewEnrollSerializer(data = data)
+                    if serializer.is_valid():
+                        serializer.save()
+                        return Response(serializer.data, status=status.HTTP_201_CREATED)
+                    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+                return Response({'message': 'You can not Enroll the Course after the course start date.'})
+            return Response({'message': 'This course has not yet been activated by the administrator, so enrollment is currently unavailable.'})  
+        return Response({'message': 'it might be you are not a user of type student.'})
 
 class LeaveCourseView(APIView):
     permission_classes = [IsAuthenticated]
 
     def post(self, request):
-        student = request.user.student
-        data = request.data.copy()
-        data['student'] = student
+        user = request.user
+        if user.user_type == 'student':
+            student = user.student
+            data = request.data.copy()
+            data['student'] = student
 
-        course_name = data.pop('course_name')
-        course_name = str(course_name[0])
+            course_name = data.pop('course_name')
+            course_name = str(course_name[0])
+            course = get_object_or_404(Course, name=course_name)
+            
+            try:
+                enrollment = Enrollment.objects.get(student=student, course=course)
+            except Enrollment.DoesNotExist:
+                return Response({'error': 'You are not enrolled in this course'}, status=status.HTTP_400_BAD_REQUEST)
+            
+            if course.start_date > datetime.date.today():
+                data['course'] = course.id
 
-        try:
-            course = Course.objects.get(name=course_name)
-        except Course.DoesNotExist:
-            return Response({'error': f"Course '{course_name}' does not exist"}, status=status.HTTP_400_BAD_REQUEST)
-        
-        try:
-            enrollment = Enrollment.objects.get(student=student, course=course)
-        except Enrollment.DoesNotExist:
-            return Response({'error': 'You are not enrolled in this course'}, status=status.HTTP_400_BAD_REQUEST)
-        
-        if course.start_date > datetime.date.today():
-            data['course'] = course.id
-
-            enrollment.delete()
-            return Response({'message': 'You have successfully left the course'}, status=status.HTTP_200_OK)
-        return Response({'message': 'You can not Leave the Course after the course start date.'})
+                enrollment.delete()
+                return Response({'message': 'You have successfully left the course'}, status=status.HTTP_200_OK)
+            return Response({'message': 'You can not Leave the Course after the course start date.'})
+        return Response({'message': 'it might be you are not a user of type student.'})
