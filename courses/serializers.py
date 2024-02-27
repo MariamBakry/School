384ca2f17e35008368b1d3966939be55be50f03e
MariamBakry <122481396+MariamBakry@users.noreply.github.com>
2024-02-27 07:05:18 +0200

    .

--- courses/serializers.py
+++ courses/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 from .models import *
 
 class CourseSerializer(serializers.ModelSerializer):
+    teacher_username = serializers.CharField(source='teacher.user.username', read_only = True)
     class Meta:
         model = Course
-        fields = '__all__'
+        fields = ['name', 'teacher_username', 'description', 'start_date', 'end_date', 'is_active']
